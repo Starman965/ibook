c5babb625bd216b7deb1311df3bcae734e98f65a
David Lewis <107167236+Starman965@users.noreply.github.com>
2024-10-29 06:37:30 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNBzrVg2HL4X5ouFJOt_6P8z3io8FcwU",
+  authDomain: "interactivebooks-39b1a.firebaseapp.com",
+  databaseURL: "https://interactivebooks-39b1a-default-rtdb.firebaseio.com",
+  projectId: "interactivebooks-39b1a",
+  storageBucket: "interactivebooks-39b1a.appspot.com",
+  messagingSenderId: "706836375736",
+  appId: "1:706836375736:web:d2074133405b606190ac4d"
+};
+
+export { firebaseConfig };
